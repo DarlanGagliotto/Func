9f18e71dffb006e6657c82479f1f30fc2f1149c9
Darlan Gagliotto <dlg_d2@hotmail.com>
2019-01-19 13:06:49 -0200

    Func com Select

--- Func/Program.cs
+++ Func/Program.cs
@@ -1,3 +1,4 @@
+using Func.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +11,24 @@ namespace Func
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV",900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+            list.Add(new Product("HD Case", 61.00));
+
+            Func<Product, string> func = NameUpper;
+
+            List<string> result = list.Select(func).ToList();
+
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
         }
+
+        public static string NameUpper(Product p) => p.Name.ToUpper();
+
     }
 }
